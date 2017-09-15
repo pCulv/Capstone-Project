f1ce8e3deb85a652295627305d62b44e7dc7e47e
PhilCulver <philculver@gmail.com>
2017-09-15 15:07:45 -0500

    Initial Commit

---
+++ app/src/main/java/com/annoyedandroid/ancora/ui/activities/EditTimerActivity.java
@@ -0,0 +1,15 @@
+package com.annoyedandroid.ancora.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.annoyedandroid.ancora.R;
+
+public class EditTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_timer);
+    }
+}
---
+++ app/src/main/java/com/annoyedandroid/ancora/ui/activities/NewTimerActivity.java
@@ -0,0 +1,15 @@
+package com.annoyedandroid.ancora.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.annoyedandroid.ancora.R;
+
+public class NewTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_timer);
+    }
+}
