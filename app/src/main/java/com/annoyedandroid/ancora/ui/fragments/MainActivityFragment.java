acfefe7464f97ad34483d1500cbdf43806302ad6
PhilCulver <philculver@gmail.com>
2017-09-16 11:22:21 -0500

    Initial Commit

--- app/src/main/java/com/annoyedandroid/ancora/ui/fragments/MainActivityFragment.java
+++ app/src/main/java/com/annoyedandroid/ancora/ui/fragments/MainActivityFragment.java
@@ -19,6 +19,8 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
