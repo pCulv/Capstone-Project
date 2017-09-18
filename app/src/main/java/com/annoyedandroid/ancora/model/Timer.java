ad94bbd08fc51848138c21c95e745bba87c3b3c6
PhilCulver <philculver@gmail.com>
2017-09-18 15:33:38 -0500

    Setup recyclerView adapter

---
+++ app/src/main/java/com/annoyedandroid/ancora/model/Timer.java
@@ -0,0 +1,8 @@
+package com.annoyedandroid.ancora.model;
+
+/**
+ * Created by phil on 9/18/17.
+ */
+
+public class Timer {
+}
--- app/src/main/java/com/annoyedandroid/ancora/ui/fragments/MainActivityFragment.java
+++ app/src/main/java/com/annoyedandroid/ancora/ui/fragments/MainActivityFragment.java
@@ -1,17 +1,39 @@
 package com.annoyedandroid.ancora.ui.fragments;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.annoyedandroid.ancora.R;
+import com.annoyedandroid.ancora.model.Timer;
+import com.annoyedandroid.ancora.ui.adapters.TimerAdapter;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 public class MainActivityFragment extends Fragment {
+
+    private TimerAdapter mAdapter;
+    private List<Timer> mTimers = new ArrayList<>();
+
+    @BindView(R.id.new_timer_fab)
+    FloatingActionButton newTimerFab;
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+
+
+
 
     public MainActivityFragment() {
     }
@@ -19,7 +41,17 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ButterKnife.bind(this.getActivity());
 
+        //code for recyclerView
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new TimerAdapter(this.getActivity(), mTimers);
+
+        mRecyclerView.setAdapter(mAdapter);
 
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
