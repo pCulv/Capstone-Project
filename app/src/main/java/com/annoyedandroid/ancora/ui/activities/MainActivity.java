cd2dd74afd3b39b8a2ddf67876d232df666e002f
PhilCulver <philculver@gmail.com>
2017-09-29 19:01:37 -0500

    Setup nav drawer in mainActivity

--- app/src/main/java/com/annoyedandroid/ancora/ui/activities/MainActivity.java
+++ app/src/main/java/com/annoyedandroid/ancora/ui/activities/MainActivity.java
@@ -5,20 +5,43 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.annoyedandroid.ancora.R;
+import com.annoyedandroid.ancora.ui.fragments.MainActivityFragment;
+import com.annoyedandroid.ancora.ui.fragments.SettingsFragment;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawer;
+    @BindView(R.id.nvView)
+    NavigationView navDrawer;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.new_timer_fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +55,58 @@ public class MainActivity extends AppCompatActivity {
         setupDrawer();
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        switch(menuItem.getItemId()) {
+            case R.id.my_timers:
+                fragmentClass = MainActivityFragment.class;
+                break;
+            case R.id.settings:
+                fragmentClass = SettingsFragment.class;
+                break;
+            case R.id.upgrade:
+                //todo build intent to open playstore for free version of app
+                Toast.makeText(this, "Open Google Play Store", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.signOut:
+                //todo: perform google sign out
+                Toast.makeText(this, "Signed Out", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                fragmentClass = MainActivityFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void setupDrawer() {
 
